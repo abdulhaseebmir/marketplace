368c76f295b025aa377fbb9d41ba56305816c651
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-02 23:34:49 +0500

    detail view added

--- item/views.py
+++ item/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from core.models import Item
 
-# Create your views here.
+def detail_view(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    context = {
+        "item": item
+    }
+    return render(request, "item/detail.html", context)
