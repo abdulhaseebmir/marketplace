f83573806e736444230e6f269dfa7f778ec52ea9
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-05 00:15:25 +0500

    create new item

--- item/urls.py
+++ item/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import detail_view
+from .views import detail_view, new_view
 
 app_name = "item"
 
 urlpatterns = [
+    path("new/", new_view, name="new"),
     path("<int:pk>/", detail_view, name="detail")
 ]
