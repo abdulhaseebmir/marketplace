77ae46ca4163f0bce067993fbdff79681100f2ec
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-05 00:23:19 +0500

    core urls

---
+++ core/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .forms import SigninFrom
+from .views import (
+    index_view, 
+    contact_view,
+    signup_view
+)
+
+app_name = "core"
+
+urlpatterns = [
+    path("", index_view, name="index"),
+    path("contact/", contact_view, name="contact"),
+    path("signup/", signup_view, name="signup"),
+    path("signin/", auth_views.LoginView.as_view(
+        authentication_form=SigninFrom,
+        template_name="core/signin.html"
+    ), name="signin")
+]
