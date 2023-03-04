98ad5a92ede0187bc02b5d1caead35fa9c596d57
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-05 01:52:10 +0500

    styling form using widgets

--- item/forms.py
+++ item/forms.py
@@ -1,7 +1,26 @@
 from django import forms
 from core.models import Item
 
+INPUT_CLASSES = "w-full py-2 px-3 rounded-lg border"
+
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ["category", "name", "description", "price", "image"]
+        widgets = {
+            "category": forms.Select(attrs={
+                "class": INPUT_CLASSES
+            }),
+            "name": forms.TextInput(attrs={
+                "class": INPUT_CLASSES
+            }),
+            "description": forms.Textarea(attrs={
+                "class": INPUT_CLASSES
+            }),
+            "price": forms.TextInput(attrs={
+                "class": INPUT_CLASSES
+            }),
+            "image": forms.FileInput(attrs={
+                "class": INPUT_CLASSES
+            })
+        }
