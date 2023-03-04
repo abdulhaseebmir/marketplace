54dd230a4ebc46c579d7e5da35ebc13c0fff689e
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-05 00:16:57 +0500

    new item view

--- item/views.py
+++ item/views.py
@@ -1,9 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+
+from .forms import NewItemForm
 from core.models import Item
 
 def detail_view(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+    
     context = {
+        "related_items": related_items,
         "item": item
     }
     return render(request, "item/detail.html", context)
+
+@login_required
+def new_view(request):
+    form = NewItemForm()
+    context = {
+        "form": form,
+        "title": "New item"
+    }
+    return render(request, "item/form.html", context)
