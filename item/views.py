8bff24457ee70e71e37869a4a4b57c0cd8d05064
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-07 13:47:47 +0500

    new item db storage

--- item/views.py
+++ item/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm
 from core.models import Item
@@ -16,7 +16,15 @@ def detail_view(request, pk):
 
 @login_required
 def new_view(request):
-    form = NewItemForm()
+    if request.method == "POST":
+        form = NewItemForm(request.POST, request.FILES) # file to be uploaded
+        if form.is_valid():
+            item = form.save(commit=False) # don't commit changes to db. Just create object
+            item.created_by = request.user # need to add created_by before adding to db
+            item.save() # commit to db
+            return redirect("item:detail", pk=item.id)
+    else:
+        form = NewItemForm()
     context = {
         "form": form,
         "title": "New item"
