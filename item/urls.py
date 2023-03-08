ce080809eedc6b500c6c7a98554105f6110d458d
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-08 16:04:32 +0500

    delete and edit paths added

--- item/urls.py
+++ item/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import detail_view, new_view
+from .views import (
+    detail_view, new_view, delete_view, edit_view
+)
 
 app_name = "item"
 
 urlpatterns = [
     path("new/", new_view, name="new"),
-    path("<int:pk>/", detail_view, name="detail")
+    path("<int:pk>/", detail_view, name="detail"),
+    path("<int:pk>/delete/", delete_view, name="delete"),
+    path("<int:pk>/edit/", edit_view, name="edit"),
 ]
