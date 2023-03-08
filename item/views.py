e8121b6d88dd733f59ba03627514ae4b628bf8e2
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-08 16:05:05 +0500

    delete and edit views added

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from core.models import Item
 
 def detail_view(request, pk):
@@ -30,3 +30,27 @@ def new_view(request):
         "title": "New item"
     }
     return render(request, "item/form.html", context)
+
+@login_required
+def edit_view(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == "POST":
+        form = EditItemForm(request.POST, request.FILES, instance=item) # file to be uploaded
+        if form.is_valid():
+            form.save()
+            return redirect("item:detail", pk=item.id)
+    else:
+        form = EditItemForm(instance=item) # so the form isn't empty
+    context = {
+        "form": form,
+        "title": "Edit item"
+    }
+    return render(request, "item/form.html", context)
+
+@login_required
+def delete_view(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect("dashboard:index")
