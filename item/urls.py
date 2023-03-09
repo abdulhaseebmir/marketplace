5b45b751dd4a247ad825d5fae708b47125f3fa68
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-09 14:49:26 +0500

    items view url added

--- item/urls.py
+++ item/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from .views import (
-    detail_view, new_view, delete_view, edit_view
+    detail_view, new_view, delete_view, edit_view, items_view
 )
 
 app_name = "item"
 
 urlpatterns = [
+    path("", items_view, name="items"),
     path("new/", new_view, name="new"),
     path("<int:pk>/", detail_view, name="detail"),
     path("<int:pk>/delete/", delete_view, name="delete"),
