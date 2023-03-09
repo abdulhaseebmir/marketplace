03b050f559488dcfac74ad1fe9a6125daace1833
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-03-09 14:49:58 +0500

    items_view added

--- item/views.py
+++ item/views.py
@@ -1,8 +1,29 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 
 from .forms import NewItemForm, EditItemForm
-from core.models import Item
+from core.models import Item, Category
+
+def items_view(request):
+    items = Item.objects.filter(is_sold=False)
+    query = request.GET.get("query", "") # Default is empty/""
+    categories = Category.objects.all()
+    category_id = request.GET.get("category", 0)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context = {
+        "items": items,
+        "query": query,
+        "categories": categories,
+        "category_id": int(category_id)
+    }
+    return render(request, "item/items.html", context=context)
 
 def detail_view(request, pk):
     item = get_object_or_404(Item, pk=pk)
